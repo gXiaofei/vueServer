73b638057bb98b87020756d9619945984a8bcb7e
xiaofei[alias] <gongxiaofei@aqa.com>
2018-09-01 17:33:52 +0800

    -

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
-
+var session = require('express-session');
 var app = express();
 
 // view engine setup
@@ -17,9 +17,14 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('login'));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+  secret: 'login',
+  cookie: {maxAge: 60000},
+  resave: true,
+  saveUninitialized: true,
+}));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
--- routes/login.js
+++ routes/login.js
@@ -2,35 +2,46 @@ var express = require('express');
 var router = express.Router();
 var db = require('../DB/db');  //require的路径是我们的db模块相对于本文件的路径
 var svgCaptcha = require('svg-captcha');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var app = express();
-app.use(cookieParser());
-app.use(session({
-    secret: '123456',
-    name: 'name',
-    cookie: {maxAge: 60000},
-    resave: false,
-    saveUninitialized: true,
-}));
+
 router.post('/', function(req, res, next) {
-    db.query('select * from user', [], function(results,fields){
-        if(req.body.username != results[0].name){
-            res.send(
-                {message: '账号或者密码错误！'}
-            )
+    var flag = true;
+    if(req.body.isSubmitErr){
+        console.log(req.body.captcha);
+        console.log( req.session.captcha);
+        if(req.body.captcha === req.session.captcha){
+            flag = true
         }else{
-            if(req.body.password != results[0].passwd ){
-                res.send(
-                    {message: '账号或者密码错误！'}
-                )
-            }else{
-                res.send(
-                    {message: '成功'}
-                )
-            }
+            flag = false
+            res.send({
+                status: 0,
+                message: '验证码不正确'
+            });
         }
-    })
+    }else{
+        flag = true
+    }
+    if(flag){
+        db.query("select * from user where username='"+ req.body.username+"'", [], function(results,fields){
+            if(results.length === 0){
+                res.send({
+                    status: 0,
+                    message: '账户或者密码不正确'
+                });
+            }else{
+                if(req.body.password === results[0].password){
+                    res.send({
+                        status: 1,
+                        message: '登陆成功'
+                    });
+                }else{
+                    res.send({
+                        status: 0,
+                        message: '账户或者密码不正确'
+                    });
+                }
+            }
+        })
+    }
 });
 // 生成验证码
 router.get('/captcha', function(req, res, next){
@@ -41,13 +52,44 @@ router.get('/captcha', function(req, res, next){
         height: 44
     }
     var captcha = svgCaptcha.create(codeConfig);
-    req.cookies.captcha = captcha.text.toLowerCase(); //存cookit用于验证接口获取文字码
+    console.log(req.session.captcha);
+    req.session.captcha = captcha.text.toLowerCase(); //存cookit用于验证接口获取文字码
+    console.log(req.session.captcha);
     var codeData = {
         img:captcha.data
     }
     res.send(codeData);
 });
+// 获取账号是否被注册
+router.post('/getUsername', function (req, res, next) {
+    db.query("select username from user where username='"+req.body.username+"'", [], function(result, fields){
+        if(result.length !== 0){
+            res.send({
+                message: 0
+            })
+        }else{
+            res.send({
+                message: 1
+            })
+        }
+    })
+});
+// 注册
+router.post('/register', function(req, res, next){
 
+    var data = ([req.body.name, req.body.password, req.body.email])
+    db.query('INSERT INTO user(username,password,email) VALUES(?,?,?)', data, function(result, fields){
+        if(result){
+            res.send({
+                message: 1
+            });
+        }else{
+            res.send({
+                message: 0
+            });
+        }
+    });
+});
 
 
 module.exports = router;
